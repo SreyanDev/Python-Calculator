2cf27d6a816f8a2df64078bc1e0ace36cb5cc6a9
Sreyan <sreyanmithra@gmail.com>
2024-11-10 12:07:51 +0530

    Create Calculator.py

---
+++ Calculator.py
@@ -0,0 +1,41 @@
+try:
+
+    a = float(input("Enter the first value: "))
+    b = float(input("Enter the second value: "))
+
+
+    operator = input("Enter the operator (+,-,*,/) : ")
+
+    match operator:
+
+        case "+":
+
+            result=a+b              #No need to assign datatype for f"The sum of {a} and {b} is {result}" like in C cuz python detect it automatically.
+
+            print(f"The sum of {a} and {b} is {result}.")
+
+        case "-":
+
+            result=a-b
+            print(f"The difference of {a} and {b} is {result}.")
+
+        case "*":
+
+            result=a*b
+            print(f"The product of {a} and {b} is {result}.")
+
+        case "/":
+
+            if b!=0:
+                result=a/b
+                print(f"The division of {a} and {b} is {result}.")
+
+            else:
+                print(f"Since the value of b is 0 the answer {a}/{b} is not defined.")
+
+
+        case _:
+            print("Enter the correct operator (+,-,*,/).")
+
+except:
+    print("Enter integer or float numbers only.")
